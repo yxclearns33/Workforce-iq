d8161c95ef423add955dac8cf310ed9886647fb2
Samuel Adebusoye <Yxclearns33@gmail.com>
2026-05-30 03:39:01 +0100

    add app screenshots

--- src/Supabase.js
+++ src/Supabase.js
@@ -1,6 +1,7 @@
+
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
+const SUPABASE_URL = "https://ieiotxbrjqddtvuqxevh.supabase.co"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllaW90eGJyanFkZHR2dXF4ZXZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMjI0MzMsImV4cCI6MjA5NDY5ODQzM30.E8rLzERY6OmIzVY3GbsGR87Vxc5Pvgx3KD0Z1PhMpYU"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
